c612993734d46eed17bc75888be1b6bda0d1daaf
chenjj100419 <61618153+chenjj100419@users.noreply.github.com>
2020-08-18 12:48:46 +0800

    update

--- Morefeatures_Java/src/main/java/me/morefeatures/morefeatures/Morefeatures.java
+++ Morefeatures_Java/src/main/java/me/morefeatures/morefeatures/Morefeatures.java
@@ -1,5 +1,6 @@
 package me.morefeatures.morefeatures;
 
+import me.morefeatures.morefeatures.commands.setname;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Morefeatures extends JavaPlugin {
@@ -7,7 +8,12 @@ public final class Morefeatures extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("[Morefeatures] register commands");
+        getCommand("setname").setExecutor(new setname());
 
+        System.out.println("[Morefeatures] register events");
+
+        System.out.println("[Morefeatures] is on Enable");
     }
 
     @Override
--- Morefeatures_Java/src/main/java/me/morefeatures/morefeatures/commands/setname.java
+++ Morefeatures_Java/src/main/java/me/morefeatures/morefeatures/commands/setname.java
@@ -1,5 +1,7 @@
 package me.morefeatures.morefeatures.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,7 +12,30 @@ public class setname implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player){
             Player player = (Player)sender;
-
+            if (args.length == 2){
+                if (player.hasPermission("morefeatures.commands.setname")) {
+                    //执行此段代码(有权限)
+                    String name = args[1];
+                    Player entity = (Player) Bukkit.getPlayerExact(args[0]);
+                    if (entity instanceof Player) {
+                        entity.setCustomName(name);
+                    }else {
+                        player.sendMessage(entity.getName()+ChatColor.GOLD+"玩家不在线!");
+                    }
+                } else {
+                    //执行此段代码(无权限)
+                    player.sendMessage(ChatColor.RED+"你没有morefeatures.commands.setname的权限!");
+                }
+            }else if (args.length == 1){
+                if (player.hasPermission("morefeatures.commands.setname")) {
+                    //执行此段代码(有权限)
+                    String name = args[1];
+                    player.setCustomName(name);
+                } else {
+                    //执行此段代码(无权限)
+                    player.sendMessage(ChatColor.RED+"你没有morefeatures.commands.setname的权限!");
+                }
+            }
         }
         return false;
     }
